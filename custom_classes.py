66fc892b4fc2828cf1e918e5df52615344143761
cihangoksu <cgksu@hotmail.com>
2026-03-10 19:33:52 +0300

    v1.0: calculator base

---
+++ custom_classes.py
@@ -0,0 +1,4 @@
+class Calculator:
+    def __init__(self):
+        # The single underscore is a convention for internal/private attributes
+        self._current_val = 0
---
+++ main.py
@@ -0,0 +1,6 @@
+from custom_classes import Calculator
+# Create an instance of the Calculator class
+my_calc = Calculator()
+
+# Access and print the internal attribute
+print(my_calc._current_val)

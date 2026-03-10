3560aba8f72c1175ca8e04233f0877750bc0191f
cihangoksu <cgksu@hotmail.com>
2026-03-10 19:45:18 +0300

    add

--- custom_classes.py
+++ custom_classes.py
@@ -2,3 +2,7 @@ class Calculator:
     def __init__(self):
         # The single underscore is a convention for internal/private attributes
         self._current_val = 0
+    def add(self, x, y):
+            """Adds two numbers and updates the internal value."""
+            self._current_val = x + y
+            return self._current_val
